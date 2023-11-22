ee6b3e19e4a31649513133da624839621085cf9c
Luis <machadosoftware@gmail.com>
2023-11-22 16:56:39 -0300

    dados api

---
+++ IncludesEndsStarts/Includess/script.js
@@ -0,0 +1,18 @@
+
+// includes, startsWith, endsWith
+
+// let nomes = ["Matheus", "Lucas", "Jose"];
+
+// //console.log(nomes.includes("Maria"));
+
+// if(nomes.includes("Matheus")){
+//   console.log("ESTA NA LISTA")
+// }else{
+//   console.log("NAO ESTA NA LISTA")
+// }
+
+let nome = "Matheus";
+
+//console.log(nome.startsWith("Mat"))
+
+console.log(nome.endsWith("eus"))
